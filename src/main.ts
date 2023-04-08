3a6bef12a6bf863ecf73766d3d62a39623bad5fb
Kevin Bourdon <kevomhd@gmail.com>
2023-04-08 12:14:19 +0200

    feat: add express

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import { Request, Response } from "express";
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.json("Hello World!");
+});
+
+app.listen(3000);
